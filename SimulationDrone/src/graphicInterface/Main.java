5eb9300319670d6bf678bd8be2aed54b03f59045
Emilien <emilien.moncan@laposte.net>
2018-06-03 17:59:53 +0200

    Animation circle + mise en page

--- SimulationDrone/src/graphicInterface/Main.java
+++ SimulationDrone/src/graphicInterface/Main.java
@@ -5,12 +5,17 @@
  */
 package graphicInterface;
 
+import javafx.animation.AnimationTimer;
 import javafx.animation.KeyFrame;
 import javafx.animation.KeyValue;
 import javafx.animation.Timeline;
 import javafx.application.Application;
+import javafx.beans.property.DoubleProperty;
+import javafx.beans.property.SimpleDoubleProperty;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
@@ -30,12 +35,13 @@ import javafx.util.Duration;
  */
 public class Main extends Application {
 
-    private int borderSize = 25;
-    private int groupBoxHeight = 500;
-    private int groupBoxWidth = 300;
-    private int screenHeight = (int) Screen.getPrimary().getVisualBounds().getHeight();
-    private int screenWidth = (int) Screen.getPrimary().getVisualBounds().getWidth();
-
+    private int BORDER_FRAME = 25;
+    private int BORDER_GROUPBOX = 16;
+    private int GROUPBOX_HEIGHT = 500;
+    private int GROUPBOX_WIDTH = 300;
+    private int SCREEN_HEIGHT = (int) Screen.getPrimary().getVisualBounds().getHeight();
+    private int SCREEN_WIDTH = (int) Screen.getPrimary().getVisualBounds().getWidth();
+    private int compt;
     @Override
     public void start(Stage applicationFrame) {
 
@@ -46,7 +52,7 @@ public class Main extends Application {
         applicationFrame.setResizable(false);
 
         BorderPane layoutSettings = new BorderPane();                           // creation layout for groupBox Settings 
-        
+
         Button btn = new Button("test");
         btn.setOnAction(new EventHandler<ActionEvent>() {
             @Override
@@ -54,23 +60,25 @@ public class Main extends Application {
                 //TODO
             }
         });
+        btn.setPrefWidth(200);
         VBox containerButton = new VBox();
+        containerButton.setAlignment(Pos.TOP_CENTER);
+        containerButton.setPadding(new Insets(BORDER_FRAME, 0, 0, 0));
+        containerButton.setSpacing(BORDER_GROUPBOX);
         containerButton.getChildren().add(new Button("1"));
         containerButton.getChildren().add(new Button("2"));
         containerButton.getChildren().add(btn);
         layoutSettings.setCenter(containerButton);
 
-
         TitledPane groupBoxSettings = new TitledPane();                         // creation groupBox Settings
         groupBoxSettings.setText("Paramètres");                                 // name of the groupBox Settings
         groupBoxSettings.setContent(layoutSettings);                            // set Layout 
         groupBoxSettings.setCollapsible(false);                                 // remove dynamic top -> bottom
-        groupBoxSettings.setLayoutX(borderSize);
-        groupBoxSettings.setLayoutY(borderSize);                                // set position of the group box settings
-        groupBoxSettings.setPrefSize(groupBoxWidth, groupBoxHeight);            // set size of the groupBox Settings        
+        groupBoxSettings.setLayoutX(BORDER_FRAME);
+        groupBoxSettings.setLayoutY(BORDER_FRAME);                                // set position of the group box settings
+        groupBoxSettings.setPrefSize(GROUPBOX_WIDTH, GROUPBOX_HEIGHT);            // set size of the groupBox Settings        
         framePanel.getChildren().add(groupBoxSettings);                         // add groupBox Settings to group
 
- 
         /*TitledPane groupBoxSimulation = new TitledPane();
         groupBoxSimulation.setText("Simulation");
         groupBoxSimulation.setCollapsible(false);
@@ -107,33 +115,63 @@ public class Main extends Application {
         pathTransition.setAutoReverse(true);
         pathTransition.play();
         groupBoxSimulation.setContent(rectPath);*/
+        
+        
 
-        final Canvas canvas = new Canvas((screenWidth - ((borderSize * 3) + groupBoxWidth)), (screenHeight - (2 * borderSize)));
-        canvas.setTranslateX((borderSize * 2) + groupBoxWidth);
-        canvas.setTranslateY(borderSize);
-        GraphicsContext gc = canvas.getGraphicsContext2D(); 
-        gc.setFill(Color.WHITE);
-        gc.fillRoundRect(borderSize, borderSize, borderSize, borderSize, borderSize, borderSize);
+        
+        DoubleProperty x  = new SimpleDoubleProperty();
+        DoubleProperty y  = new SimpleDoubleProperty();
+
+        Timeline timeline = new Timeline(
+            new KeyFrame(Duration.seconds(0),
+                    new KeyValue(x, 0),
+                    new KeyValue(y, 0)
+            ),
+            new KeyFrame(Duration.seconds(3),
+                    new KeyValue(x, (SCREEN_WIDTH - ((BORDER_FRAME * 3) + GROUPBOX_WIDTH))),
+                    new KeyValue(y, (SCREEN_HEIGHT - (2 * BORDER_FRAME)))
+            )
+        );
+        timeline.setAutoReverse(true);
+        timeline.setCycleCount(Timeline.INDEFINITE);
+        
+                final Canvas canvas = new Canvas((SCREEN_WIDTH - ((BORDER_FRAME * 3) + GROUPBOX_WIDTH)), (SCREEN_HEIGHT - (2 * BORDER_FRAME)));
+        canvas.setTranslateX((BORDER_FRAME * 2) + GROUPBOX_WIDTH);
+        canvas.setTranslateY(BORDER_FRAME);
+        GraphicsContext gc = canvas.getGraphicsContext2D();
         gc.setFill(Color.BLUE);
-        gc.fillRoundRect(100, 100, 100, 100, 100, 100);
-         /*gc.get
+        gc.fillRect(100,100, BORDER_FRAME, BORDER_FRAME);
+        gc.setFill(Color.RED);
+        gc.fillRect(500, 500, BORDER_FRAME, BORDER_FRAME);
+        compt = 100;
+        /*gc.setFill(Color.YELLOW);
+        gc.fillRoundRect(compt, compt, BORDER_FRAME, BORDER_FRAME, BORDER_FRAME, BORDER_FRAME);*/
         
-        
-        
-        final Timeline timeline = new Timeline();
- timeline.setCycleCount(2);
- timeline.setAutoReverse(true);
- timeline.getKeyFrames().add(new KeyFrame(Duration.millis(5000),
-   new KeyValue (gc.getCanvas(), 25)));
- timeline.play();
- */
- 
- 
+        AnimationTimer timer = new AnimationTimer() {
+            @Override
+            public void handle(long now) {
+                GraphicsContext gc = canvas.getGraphicsContext2D();
+                gc.setFill(Color.YELLOW);
+                gc.fillRoundRect(compt, compt, BORDER_FRAME, BORDER_FRAME, BORDER_FRAME, BORDER_FRAME);
+                ++compt;
+            }
+        };
+        /*final Timeline timeline = new Timeline();
+        timeline.setCycleCount(2);
+        timeline.setAutoReverse(true);
+        timeline.getKeyFrames().add(new KeyFrame(Duration.millis(5000),
+          new KeyValue (gc.getCanvas(), 25)));
+        timeline.play();
+         */
+
         //gc.fillRect(0,0,(screenWidth - ((borderSize * 3) + groupBoxWidth)), (screenHeight - (2 * borderSize)));*/
         framePanel.getChildren().add(canvas);
 
         applicationFrame.setScene(frameScene);                                  // add scene to frame
         applicationFrame.show();                                                // show the frame
+        
+                timer.start();
+        timeline.play();
 
     }
 }
